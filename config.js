3e675b729bdb506746df9833087c89de5abc1ec6
itz-Hiru <hirumithagithub@gmail.com>
2025-03-26 14:52:46 +0530

    Created simple backend server

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = 3000;
+
+export const mongoDBURL =
+  "mongodb+srv://hirumitha:root2003@book-review.zgndjpg.mongodb.net/?retryWrites=true&w=majority&appName=Book-Review";
---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const mongoDBURL = process.env.MONGODB_URL;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(200).send("Welcome to Book Review System");
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected to database.");
+
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection error:", error);
+  });
